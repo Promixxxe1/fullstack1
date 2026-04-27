51321493ba2b066e92566d6c368e45a9b063ae17
Promixxxe1 <iheanachopromise068@gmail.com>
2026-04-27 23:26:36 +0100

    Fix case-sensitive import paths for Linux/Vercel compatibility

--- admin/admin/src/App.jsx
+++ admin/admin/src/App.jsx
@@ -4,7 +4,7 @@ import { Routes, Route } from "react-router-dom";
 import Add from "./pages/Add";
 import List from "./pages/List";
 import Orders from "./pages/Orders";
-import SideBar from "./components/sideBar";
+import Sidebar from "./components/Sidebar";
 import Login from "./components/Login";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -31,7 +31,7 @@ const App = () => {
           <Navbar setToken={setToken} />
           <hr />
           <div className="flex w-full ">
-            <SideBar />
+            <Sidebar />
             <div className="w-[70%] mx-auto ml-[max(5vw, 25px)] my-8 text-gray-600 text-base">
               <Routes>
                 <Route path="/add" element={<Add token={token} />} />
